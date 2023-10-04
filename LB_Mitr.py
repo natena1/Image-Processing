c346bfc8a19fba78e4d4fab2044ef6a1b631e560
natena1 <131946624+natena1@users.noreply.github.com>
2023-10-04 21:56:52 +0300

    Add files via upload

---
+++ LB_Mitr.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 28 19:30:50 2023
+
+@author: Acer
+"""
+# 1 способ
+x1 = np.array([np.arange(5), np.arange(5), np.arange(5),np.arange(5),np.arange(5)])
+
+        
+# 2 способ
+x2 = np.zeros((5,5))
+x2 += np.arange(5)
+
